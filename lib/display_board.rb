93550dffc8e725a758482c0e42607fcadc9c71eb
Sara Schwarz <saramschwarz@gmail.com>
2017-10-11 12:29:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board(board)
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["O", "X ", "X", "X ", "X ", "O ", "O ", "O ", "X "]
  puts " #{board[0]} | #{board[1]} | #{board[2]} "
  puts "-----------"
  puts " #{board[3]} | #{board[4]} | #{board[5]} "
